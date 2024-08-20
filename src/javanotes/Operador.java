02a084fc63c4d57fb87ae63920f4629014ec4483
kevin duque monsalve <kevinduquemonsalve8@mail.com>
2024-08-20 07:25:39 -0500

    operadores
    operadores aritmeticos

---
+++ src/javanotes/Operador.java
@@ -0,0 +1,26 @@
+package javanotes;
+
+public class Operador {
+    public static void main(String[] args) {
+        //hay tipo 5 de operadores: aritmeticos:(+,-,*,/,%)
+        int num1 = 50;
+
+        int num2 = 35;
+
+        int sum= num1 + num2;
+
+        System.out.println(sum);
+
+        int rest = num1 - num2;
+
+        System.out.println(rest);
+
+        int mult = num1 * num2;
+
+        System.out.println(mult);
+
+        int div = num1 / num2;
+
+        System.out.println(div);
+    }
+}
